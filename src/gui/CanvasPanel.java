3042818cdc89ab229c98caa3359ee9e2f1d66975
Matúš Ráček <matesfcracek@gmail.com>
2025-05-06 13:37:18 +0200

    version 0.0.3
    updated commentary, last (I hope), CanvasPanel

--- src/gui/CanvasPanel.java
+++ src/gui/CanvasPanel.java
@@ -17,23 +17,38 @@ public class CanvasPanel extends JPanel {
     private ArrayList<Point> pointList;
     private int brushSize = 0;
 
+    /**
+     * Vytvori instanciu pre platno s velkostou stetca
+     * @param brushSize int
+     */
     public CanvasPanel(int brushSize) {
         this.pointList = new ArrayList<>();
         this.brushSize = brushSize;
     }
 
+    /**
+     * Vycisti platno
+     */
     public void reset() {
         this.pointList.clear();
         this.repaint();
     }
 
+    /**
+     * Kresli na platno podla x, y
+     * @param x int
+     * @param y int
+     */
     public void draw(int x, int y) {
         this.pointList.add(new Point(x, y));
         this.repaint();
     }
 
 
-
+    /**
+     * Metoda ktora je refreshnuta vzdy ked sa zavola repaint(), pomocou Graphics komponentu
+     * @param g Graphics
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -51,16 +66,33 @@ public class CanvasPanel extends JPanel {
         }
 
     }
+
+    /**
+     * Nakresli bod podla suradnic na platno s velkostou stetca
+     * @param p bod so suradnicami
+     * @param g grapficky komponent
+     * @param brushSize velkost stetca
+     */
     public static void paintPoint(Point p, Graphics2D g, int brushSize) {
         g.setColor(Color.black);
         g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.45f));
         g.fillRect(p.x, p.y, brushSize, brushSize);
     }
 
+    /**
+     * Nastavime atribut pre zoznam bodov
+     * @param pointList ArrayList<Point>
+     */
     public void setPointList(ArrayList<Point> pointList) {
         this.pointList = pointList;
     }
 
+    /**
+     * Getter na atribut, zoznam bodov
+     * @param width int
+     * @param height int
+     * @return ArrayList<Point>
+     */
     public ArrayList<Point> getImagePoints(int width, int height) {
         ArrayList<Point> scaled = new ArrayList<>();
         for (Point p : this.pointList) {
@@ -71,6 +103,10 @@ public class CanvasPanel extends JPanel {
         return scaled;
     }
 
+    /**
+     * Vrati obrazok, ktory sa aktualne nachadza na platne
+     * @return BufferedImage
+     */
     public BufferedImage getImage() {
         var image = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
         var g = image.createGraphics();
